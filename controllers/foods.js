d8501c544a86f9ff16ec03843f8d9c2a04a27b44
Fabian Silva <silvafabian74@yahoo.com>
2022-03-07 13:43:04 -0500

    complete create functionality

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,3 +1,4 @@
+import { shallowCopy } from 'ejs/lib/utils'
 import { Food } from '../models/food.js'
 
 function index(req, res) {
@@ -14,7 +15,36 @@ function index(req, res) {
     res.redirect('/foods')
   })
 }
+function create(req, res) {
+  console.log('this is create')
+  req.body.owner = req.user.profile._id
+  Food.create(req.body)
+  .then(food => {
+    res.redirect('/foods')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/foods')
+  })
+}
+
+// function show(req, res) {
+//   Food.findById(req.params.id)
+//   .populate("owner")
+//   .then(food => {
+//     res.render('foods/show', {
+//       food,
+//       title: "show"
+//     })
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.redirect('/foods')
+//   })
+// }
 
 export {
-  index
+  index,
+  create,
+  // show,
 }
--- routes/foods.js
+++ routes/foods.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import * as foodsCtrl from '../controllers/foods.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 // GET - localhost:3000/foods
 router.get('/', foodsCtrl.index)
+//GET - localhost:3000/foods/:id
+// router.get('/:id', foodsCtrl.show)
+
+//POST - localhost:3000/foods
+router.post('/', isLoggedIn, foodsCtrl.create)
 
 export {
   router,
