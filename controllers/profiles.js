5da8e9e2b2b4583b945ff34af077a8e212f538a5
Fabian Silva <silvafabian74@yahoo.com>
2022-03-07 20:08:33 -0500

    create profile view folder

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,6 +2,17 @@ import { Profile } from '../models/profile.js'
 
 function index(req, res) {
   console.log("PROFILES")
+  Profile.find({})
+  .then(profiles => {
+    res.render('profiles/index', {
+      profiles,
+			title: "Profiles"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  })
 }
 
 export {
